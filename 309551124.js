ffcc0e9d5a24bd5a7c9fda681ff92539a6fcb41e
ERIC <ERIC@email.com>
2020-12-19 23:12:03 +0800

    修正長寬

--- 309551124.js
+++ 309551124.js
@@ -3,9 +3,9 @@
 // set the dimensions and margins of the graph
 
 console.log("fl = 8")
-var margin = {top: 200, right: 100, bottom: 0, left: 100},
-    width = 1400 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+var margin = {top: 200, right: 100, bottom: 200, left: 100},
+    width = window.screen.width - margin.left - margin.right,
+    height = window.screen.height - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
