bb9c5496c485af1247c54a18899c9e3f3dc1d10e
ERIC <ERIC@email.com>
2021-01-12 22:41:14 +0800

    xx

--- 國家公園/國家公園.js
+++ 國家公園/國家公園.js
@@ -2,7 +2,7 @@
 
 // set the dimensions and margins of the graph
 
-console.log("fl = 4")
+console.log("fl = 5")
 var margin = {top: 200, right: 100, bottom: 150, left: 100},
     width = window.screen.width - margin.left - margin.right,
     height = window.screen.height - margin.top - margin.bottom;
@@ -20,7 +20,7 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Parse the Data
-  d3.csv("./國家公園.csv", function(data) {
+  d3.csv("國家公園.csv", function(data) {
 
 
   // List of groups = header of the csv files
@@ -33,12 +33,12 @@ var svg = d3.select("#my_dataviz")
   var tickLabels = [2012, 2013, 2014 , 2015, 2016, 2017, 2018, 2019];
   var tickmonth = ['5月', '9月']
   svg.append("g")
-    .attr("transform", "translate(50," + height + ")")
-    .call(d3.axisBottom(x).tickSize(-height).tickValues([2012.076923, 2013.076923, 2014.076923, 2015.076923, 2016.076923, 2017.076923, 2018.076923, 2019.076923]).tickFormat(function(d,i){ return tickLabels[i] }))
+    .attr("transform", "translate(50," + height*0.9 + ")")
+    .call(d3.axisBottom(x).tickSize(-height*0.95).tickValues([2012.076923, 2013.076923, 2014.076923, 2015.076923, 2016.076923, 2017.076923, 2018.076923, 2019.076923]).tickFormat(function(d,i){ return tickLabels[i] }))
     .select(".domain").remove()
 
   svg.append("g")
-    .attr("transform", "translate(50," + height*0.8 + ")")
+    .attr("transform", "translate(50," + height*0.9 + ")")
     .call(d3.axisBottom(x).tickSize(-height*.9).tickValues([2012.384615, 2012.692308]).tickFormat(function(d,i){ return tickmonth[i] }))
     .select(".domain").remove()
   // Customization
