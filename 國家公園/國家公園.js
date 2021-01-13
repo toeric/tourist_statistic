e01496b96411b622457a8e72fff89dcf9313e651
ERIC <ERIC@email.com>
2021-01-13 23:35:02 +0800

    new

--- 國家公園/國家公園.js
+++ 國家公園/國家公園.js
@@ -2,7 +2,7 @@
 
 // set the dimensions and margins of the graph
 
-console.log("fl = change img10")
+console.log("fl = change img11")
 var margin = {top: 200, right: 100, bottom: 150, left: 100},
     width = window.screen.width - margin.left - margin.right,
     height = window.screen.height - margin.top - margin.bottom;
@@ -27,9 +27,9 @@ function change_img(obj){
   var imgs=document.getElementById("imgs");
   // var val=pic.options;
   // console.log(val[index].text)
-  imgs.src="img/"+index.toString();   ////動態設定img的src屬性值，隨著滑鼠的選擇切換圖片
+  imgs.src="img/"+index.toString()+".png";   ////動態設定img的src屬性值，隨著滑鼠的選擇切換圖片
   // imgs.src="img/"+pic.value;
-  console.log("img/"+index.toString())
+  console.log("img/"+index.toString()+".png")
   console.log(imgs.src)
  }
 
